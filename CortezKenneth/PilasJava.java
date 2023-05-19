df0d2a62fa644e6e9348ef0e75859b0e43744f03
Kenneth Cortez <kennethcortez19@hotmail.com>
2023-05-19 10:00:03 -0500

    java

---
+++ CortezKenneth/PilasJava.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class PilasJava{
+    public static void main(String[] args) {
+        Stack<Integer> pila = new Stack<>();
+
+        pila.push(10);
+        pila.push(20);
+        pila.push(30);
+
+        while (!pila.empty()) {
+            System.out.println(pila.peek() +" ");
+            pila.pop();
+        }
+    }
+}
