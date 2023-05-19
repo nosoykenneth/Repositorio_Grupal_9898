a59f84ea36c5c47d789f3217935242b097bf6bcc
Kenneth Cortez <kennethcortez19@hotmail.com>
2023-05-19 09:43:15 -0500

    ejemplos pilas

---
+++ CortezKenneth/PilasClase.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    stack<int> pila;
+
+    pila.push(10);
+    pila.push(20);
+    pila.push(30);
+
+    while (!pila.empty())
+    {
+        cout<<pila.top()<<"";
+        pila.pop();
+    }
+
+    return 0;
+    
+    
+
+}
