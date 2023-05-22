26ad500a52a89ea82a88f38cc35848291a1d5a61
Kenneth Cortez <kennethcortez19@hotmail.com>
2023-05-22 10:02:28 -0500

    Taller Python

---
+++ CortezKenneth/Taller Python/Tipos de Datos/compuestos.py
@@ -0,0 +1,13 @@
+lista = []
+
+print(type(lista))
+
+lista = ["Ecuador","Colombia","Brasil"]
+print(lista)
+
+lista = ["Juan",45,6.5,True,["Ecuador","Colombia","Brasil"],"a"]
+print(lista)
+
+lista = ["Ecuador","Colombia","Brasil"]
+lista.append("Mexico")
+print(lista)
--- CortezKenneth/Taller Python/Tipos de Datos/primitivos.py
+++ CortezKenneth/Taller Python/Tipos de Datos/primitivos.py
@@ -8,3 +8,11 @@ sueldo = 0.45
 mayorEdad = True
 
 complejo = 4j
+
+#help(type)
+
+print(type(nombre))
+print(type(edad))
+print(type(sueldo))
+print(type(mayorEdad))
+print(type(complejo))
