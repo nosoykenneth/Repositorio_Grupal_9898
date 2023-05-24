7a94decbc72c94f248521bb0428ff4e931c6007e
Kenneth Cortez <kennethcortez19@hotmail.com>
2023-05-24 10:02:40 -0500

    Taller Python

---
+++ CortezKenneth/Taller Python/Condicionales/if.py
@@ -0,0 +1,28 @@
+if 2>8:
+    print("2 es mayor que 8")
+elif 10>20:
+    print("10 es menor a 20")
+elif 100<=80:
+    print("100 es mayor o igual que 80")
+elif 80>=100:
+    print("80 es menor o igual que 100")
+elif 100!=100:
+    print("100 es diferente de 20")
+elif 100==101:
+    print("100 es igual a 100")
+else:
+    print("No se cumplio ninguna condicion anterior")
+
+#Operadores ternarios
+if 2<10: print("2 es menor a 5")
+
+print("Se imprime si la condicion es verdadera") if 10>2 else print("Se imprime si la condicion es falsa")
+
+#And
+
+if 2<9 and 10<20:
+    print("2 es mayor que 9 y 10 es menor que 20")
+
+#Or
+if 100<1000 or 20 > 900:
+    print("100 es menor que 100 o 20 es mayor que 900")
